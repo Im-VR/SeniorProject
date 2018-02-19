68817636934915891a48b0ee6daed6263ce794ca
Afnan <Afnan.Redwan@gmail.com>
2018-02-19 15:02:56 +0300

    Afnan

---
+++ IM-/Assets/Rotate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Rotate : MonoBehaviour {
+
+    public Text score;
+    private int count;
+
+	//playerController pc = new playerController();
+	//countController cc = new countController();
+	// Use this for initialization
+	void Start () {
+        count = 0;
+       // updateScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+	}
+
+	/*public void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("pickUps"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+            updateScore();
+
+		}	
+	}
+
+    public void updateScore()
+    {
+        score.text = "Score: " + count;
+
+    }*/
+}
---
+++ IM-/Assets/playerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour {
+
+    public int count = 0 ;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		//updateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     //   updateScore();
+	}
+   
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickUps"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            Debug.Log(count);
+            updateScore();
+        }
+    }
+
+/*	public void incScore(){
+		count++;
+		updateScore ();
+	}*/
+
+	public void updateScore(){
+		scoreText.text = "Score: " + count.ToString();
+
+	}
+
+
+}
