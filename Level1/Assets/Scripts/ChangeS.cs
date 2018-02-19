ad45574810017064dbc15a7c954b5d6ee98a66c2
Im-VR <Im-VR@github.com>
2018-02-19 12:59:29 +0300

    Lvl1+Menus

---
+++ Level1/Assets/Scripts/ChangeS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	public void ChangetoScene(int s){
+
+		SceneManager.LoadScene (s);
+
+	}
+
+
+
+}
---
+++ Level1/Assets/toggleVrMode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class toggleVrMode : MonoBehaviour {
+
+	public int score;
+	public Text scoreText;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		updateScore ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+	public void incScore(){
+		scoreText.text = "Score:gggggg " + score.ToString();
+		score++;
+		updateScore ();
+	}
+
+	public void updateScore(){
+		scoreText.text = "Score: " + score.ToString();
+
+	}
+
+	public void moveUp(){
+		transform.position += new Vector3 (0f, 1f, 0f);
+
+	}
+
+}
